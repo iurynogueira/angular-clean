fd4d5c8b070ec5960e5ec2fe15e454c39658a1a4
Iury Nogueira <iunog@hotmail.com>
2022-06-17 11:50:50 -0300

    :hammer: added sut to manga entity spec #12

--- src/app/domain/entities/manga/manga-entity.spec.ts
+++ src/app/domain/entities/manga/manga-entity.spec.ts
@@ -1,20 +1,19 @@
 import { MangaEntity } from './manga-entity';
 
+const sut = ():MangaEntity => {
+  return new MangaEntity({
+    name: 'one piece',
+    total_pages: 1000,
+  });
+};
+
 describe('Mangá', () => {
   it('should create instance mangá entity', () => {
-    const manga = new MangaEntity({
-      name: 'one piece',
-      total_pages: 1000,
-    });
-    expect(manga).toBeTruthy();
+    expect(sut()).toBeTruthy();
   });
 
   it('should add bookmark in mangá', () => {
-    const manga = new MangaEntity({
-      name: 'one piece',
-      total_pages: 1000,
-    });
-
+    const manga = sut();
     manga.bookmarkPage(30);
     expect(manga.actual_page).toBe(30);
   });
