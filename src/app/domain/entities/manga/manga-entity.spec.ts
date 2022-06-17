6f740cbb9299f7d8f5bcb5b9dc8ded94fe87982a
Iury Nogueira <nogueiraiury20@gmail.com>
2022-06-17 14:04:16 -0300

    Props to entity (#14)
    * :sparkles: add: entity constructor #12
    
    * :hammer: improve code #12
    
    * :hammer: improve code #12
    
    * :hammer: added sut to manga entity spec #12
    
    * :hammer: added sut to manga entity spec #12

--- src/app/domain/entities/manga/manga-entity.spec.ts
+++ src/app/domain/entities/manga/manga-entity.spec.ts
@@ -1,7 +1,20 @@
 import { MangaEntity } from './manga-entity';
 
+const sut = (): MangaEntity => {
+  return new MangaEntity({
+    name: 'one piece',
+    total_pages: 1000,
+  });
+};
+
 describe('Mangá', () => {
   it('should create instance mangá entity', () => {
-    expect(new MangaEntity()).toBeTruthy();
+    expect(sut()).toBeTruthy();
+  });
+
+  it('should add bookmark in mangá', () => {
+    const manga = sut();
+    manga.bookmarkPage(30);
+    expect(manga.actual_page).toBe(30);
   });
 });
