728ea0e6b0377d34f3b1d0029c138426ace35a06
Danilo Alves <dannalves111@gmail.com>
2022-06-18 12:26:36 -0300

    test: get local storage

--- src/app/infra/client-storage/local-storage.spec.ts
+++ src/app/infra/client-storage/local-storage.spec.ts
@@ -12,6 +12,10 @@ describe('LocalStorageService', () => {
     localStorageService = TestBed.inject(LocalStorageService);
   });
 
+  afterEach(() => {
+    localStorage.clear();
+  });
+
   describe('Set', () => {
     it('should add value in local storage', () => {
       localStorageService.set('name', 'angular');
@@ -26,5 +30,24 @@ describe('LocalStorageService', () => {
     it.todo(
       'should throw error when local storage cannot set the provided value'
     );
+  });
+  describe('Get', () => {
+    it('should get a single value in local storage', () => {
+      localStorage.setItem('test', JSON.stringify('get'));
+      expect(localStorageService.get('test')).toBe('get');
+    });
+    it('should get an object value in local storage', () => {
+      const myObject = { value: 1 };
+      localStorage.setItem('test', JSON.stringify(myObject));
+      expect(localStorageService.get('test')).toEqual(myObject);
+    });
+    it('should return null when a given key is not setted', () => {
+      expect(localStorageService.get('another test')).toBeNull();
+    });
+    it('should throw error when no key is provided to get', () => {
+      expect(() => {
+        localStorageService.get('');
+      }).toThrowError('Error getting data in localStorage: key not provided');
+    });
   });
 });
