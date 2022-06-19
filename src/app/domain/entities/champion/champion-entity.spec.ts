580ed1efe91dbe0bdeb071dccdc8f69d48ed10d7
MarcosOliveir4 <marcosassis071@gmail.com>
2022-06-18 21:06:44 -0300

    :bulb: refactor in create of champion

--- src/app/domain/entities/champion/champion-entity.spec.ts
+++ src/app/domain/entities/champion/champion-entity.spec.ts
@@ -1,17 +1,18 @@
 import { ChampionEntity } from './champion-entity';
 
+const champion = {
+  name: 'Aatrox',
+  key: '266',
+  tags: ['Fighter', 'Tank'],
+  stats: {
+    hp: 650,
+    armor: 38,
+    attackdamage: 60,
+    attackspeed: 0.651,
+  },
+};
 const sut = (): ChampionEntity => {
-  return new ChampionEntity({
-    name: 'Aatrox',
-    key: '266',
-    tags: ['Fighter', 'Tank'],
-    stats: {
-      hp: 650,
-      armor: 38,
-      attackdamage: 60,
-      attackspeed: 0.651,
-    },
-  });
+  return new ChampionEntity(champion);
 };
 
 describe('', () => {
@@ -20,24 +21,18 @@ describe('', () => {
   });
 
   it('should validate name', () => {
-    expect(sut().name).toBe('Aatrox');
+    expect(sut().name).toBe(champion.name);
   });
 
   it('should validate key', () => {
-    expect(sut().id).toBe('266');
+    expect(sut().id).toBe(champion.key);
   });
 
   it('should validate tags', () => {
-    expect(sut().tags).toEqual(['Fighter', 'Tank']);
+    expect(sut().tags).toEqual(champion.tags);
   });
 
   it('should validate stats', () => {
-    const stats = {
-      hp: 650,
-      armor: 38,
-      attackdamage: 60,
-      attackspeed: 0.651,
-    };
-    expect(sut().stats).toEqual(stats);
+    expect(sut().stats).toEqual(champion.stats);
   });
 });
