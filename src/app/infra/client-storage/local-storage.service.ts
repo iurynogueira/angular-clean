763596f24a202b7c3221bb14f7a7af72bb17d210
Danilo Alves <dannalves111@gmail.com>
2022-06-20 17:04:29 -0300

    Create local persistance (#21)
    * add: client storage interface
    
    * add: local storage set tests and implementation
    
    * refactor: group tests
    
    * test: get local storage
    
    * add: get local storage implementation
    
    * refactor: functions returns on interface
    
    * test: remove from local storage
    
    * add: remove from local storage implementation
    
    * test: clear from local storage
    
    * add: clear from local storage implementation
    
    * refactor: easter eggs
    
    * test: add another case to set tests
    
    * refactor: adds safe any

---
+++ src/app/infra/client-storage/local-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { SafeAny } from 'src/safeAny';
+import IClientStorage from './iclient-storage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService implements IClientStorage {
+  set(key: string, data: SafeAny) {
+    if (key) {
+      try {
+        localStorage.setItem(key, JSON.stringify(data));
+      } catch (error) {
+        throw new Error(`Error saving to localStorage: ${error}`);
+      }
+      return;
+    }
+    throw new Error('Error saving to localStorage: key not provided');
+  }
+
+  get(key: string): SafeAny {
+    if (key) {
+      const value = localStorage.getItem(key);
+      if (value) {
+        return JSON.parse(value);
+      }
+      return null;
+    }
+    throw new Error('Error getting data in localStorage: key not provided');
+  }
+
+  remove(key: string) {
+    if (key) {
+      localStorage.removeItem(key);
+      return;
+    }
+    throw new Error('Error removing data in localStorage: key not provided');
+  }
+
+  clear() {
+    localStorage.clear();
+  }
+}
