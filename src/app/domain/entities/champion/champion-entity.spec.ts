7130cfb841d347381acb5dd6c1df9a03d7a12ac7
Iury Nogueira <iunog@hotmail.com>
2022-06-23 14:29:44 -0300

    :sparkles: add compare champions #34

--- src/app/domain/entities/champion/champion-entity.spec.ts
+++ src/app/domain/entities/champion/champion-entity.spec.ts
@@ -25,4 +25,72 @@ describe('Champion Entity', () => {
   it('should validate stats', () => {
     expect(sut().stats).toEqual(championMock().stats);
   });
+
+  it('should compare with other champion and return the differences and witout tags', () => {
+    const ezrealWithHp = new ChampionEntity({
+      key: '81',
+      name: 'Ezreal',
+      stats: {
+        armor: 24,
+        attackdamage: 60,
+        attackspeed: 0.625,
+        hp: 900,
+      },
+      tags: ['Mage', 'Marksman'],
+    });
+
+    const volibear = new ChampionEntity({
+      key: '106',
+      name: 'Volibear',
+      stats: {
+        armor: 31,
+        attackdamage: 60,
+        attackspeed: 0.625,
+        hp: 650,
+      },
+      tags: ['Fighter', 'Tank'],
+    });
+
+    expect(ezrealWithHp.compareWith(volibear)).toEqual({
+      armor: 'smaller',
+      attackdamage: 'equal',
+      attackspeed: 'equal',
+      hp: 'bigger',
+      tag: false,
+    });
+  });
+
+  it('should compare two champions with one same tag', () => {
+    const ezrealWithHp = new ChampionEntity({
+      key: '81',
+      name: 'Ezreal',
+      stats: {
+        armor: 24,
+        attackdamage: 60,
+        attackspeed: 0.625,
+        hp: 900,
+      },
+      tags: ['Marksman', 'Mage'],
+    });
+
+    const annie = new ChampionEntity({
+      key: '106',
+      name: 'Annie',
+      stats: {
+        armor: 19,
+        attackdamage: 60,
+        attackspeed: 0.625,
+        hp: 650,
+      },
+      tags: ['Mage'],
+    });
+
+    expect(ezrealWithHp.compareWith(annie)).toEqual({
+      armor: 'bigger',
+      attackdamage: 'equal',
+      attackspeed: 'equal',
+      hp: 'bigger',
+      tag: true,
+    });
+  });
 });
