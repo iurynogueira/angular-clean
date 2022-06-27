bcedb671dad02d9a18e59b028e588375fd1773d6
Iury Nogueira <iunog@hotmail.com>
2022-06-27 09:33:42 -0300

    :sparkles: feat: add use case to shot champion #18

---
+++ src/app/domain/usecases/HintChampion.spec.ts
@@ -0,0 +1,67 @@
+import { LocalStorageService } from 'src/app/infra/client-storage/local-storage.service';
+import ServiceFactory from 'src/app/infra/factory/ServiceFactory';
+import ServiceFactoryHttp from 'src/app/infra/factory/ServiceFactoryHttp';
+import AxiosAdapter from 'src/app/infra/http/AxiosAdapter';
+import { championMock } from 'src/app/mock';
+import { championMockEzreal } from 'src/app/mock/championMockEzreal';
+import { environment } from 'src/environments/environment';
+import {
+  ChampionEntity,
+  CompareState,
+} from '../entities/champion/champion-entity';
+import HintChampion from './HintChampion';
+
+let hintChampion: HintChampion;
+let localStorageService: LocalStorageService;
+let serviceFactory: ServiceFactory;
+
+const mockChampionInLocal = {
+  lastChampions: [],
+  pickedChampion: championMockEzreal().name,
+};
+
+beforeEach(() => {
+  localStorageService = new LocalStorageService();
+  serviceFactory = new ServiceFactoryHttp(
+    new AxiosAdapter(),
+    environment.serverUrl
+  );
+  hintChampion = new HintChampion(localStorageService, serviceFactory);
+  localStorageService.set('champions', mockChampionInLocal);
+});
+
+describe('HintChampion', () => {
+  it('should return failure when the champions its not equal', async () => {
+    const compareResultFail = await hintChampion.execute({
+      champion: new ChampionEntity(championMock()),
+    });
+
+    expect(compareResultFail).toEqual({
+      result: false,
+      armor: CompareState.Bigger,
+      attackdamage: CompareState.Equal,
+      attackspeed: CompareState.Bigger,
+      hp: CompareState.Bigger,
+      tag: false,
+    });
+  });
+
+  it('should return successe when the champions its equal', async () => {
+    const compareResultSuc = await hintChampion.execute({
+      champion: new ChampionEntity(championMockEzreal()),
+    });
+
+    expect(compareResultSuc).toEqual({
+      result: true,
+      armor: CompareState.Equal,
+      attackdamage: CompareState.Equal,
+      attackspeed: CompareState.Equal,
+      hp: CompareState.Equal,
+      tag: true,
+    });
+  });
+
+  afterAll(() => {
+    localStorageService.clear();
+  });
+});
