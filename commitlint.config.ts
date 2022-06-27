763504700f8c05bde153fdd85bf15ba8c8e7235d
mizek1 <dannalves111@gmail.com>
2022-06-27 17:18:11 -0300

    chore: add commitlint and commitizen
    fix #30

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+import { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'improv',
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
+
+module.exports = Configuration;
