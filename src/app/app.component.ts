d32cfd9cde619efde5f18371aff441d146900af6
Iury Nogueira <iunog@hotmail.com>
2022-06-30 16:45:21 -0300

    feat: add page to select #42

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import ServiceFactoryHttp from './infra/factory/ServiceFactoryHttp';
-import AxiosAdapter from './infra/http/AxiosAdapter';
+import ChampionController from './controller/ChampionController';
+import { ChampionEntity } from './domain/entities/champion/champion-entity';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +8,37 @@ import AxiosAdapter from './infra/http/AxiosAdapter';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  public champions: ChampionEntity[] = [];
+  public championSelected!: ChampionEntity;
+  public hintResult = {};
+
+  private championController = new ChampionController();
+
+  async hint() {
+    // const compareResult = this.championSelected.compareWith(this.championSelected);
+    const result = await this.championController.hint(this.championSelected);
+    console.log('result ->', result);
+    this.hintResult = result;
+  }
+
   async ngOnInit() {
-    const serviceFactory = new ServiceFactoryHttp(
-      new AxiosAdapter(),
-      environment.serverUrl
+    // const serviceFactory = new ServiceFactoryHttp(
+    //   new AxiosAdapter(),
+    //   environment.serverUrl
+    // );
+    // const championService = serviceFactory.createChampionService();
+
+    // mock new game
+    localStorage.setItem(
+      'champions',
+      JSON.stringify({
+        lastChampions: [],
+        pickedChampion: 'Ezreal',
+      })
     );
-    const championService = serviceFactory.createChampionService();
-    await championService.list();
+
+    const championService = this.championController.getService();
+    this.champions = await championService.list();
+    this.championSelected = this.champions[0];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
